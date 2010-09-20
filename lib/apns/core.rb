c6766e93f60cd879d2058c3d94689333e53889a6
Paul Gebheim <paul@justin.tv>
2010-09-20 12:36:33 -0700

    Add license to source header. Update license to include 2010 Justin.tv. Remove now-unused notification.rb from lib/apns, lib/apns.rb, and apns.gemspec

--- lib/apns/core.rb
+++ lib/apns/core.rb
@@ -1,3 +1,26 @@
+# Copyright (c) 2009 James Pozdena, 2010 Justin.tv
+#  
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#  
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#  
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+ 
 module APNS
   require 'socket'
   require 'openssl'
@@ -104,10 +127,6 @@ module APNS
     else
       raise "Message needs to be either a hash or string"
     end
-  end
-  
-  def self.apns_from_hash(hash)
-    hash.to_json
   end
   
   def self.with_notification_connection(&block)
